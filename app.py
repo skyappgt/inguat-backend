4e014976d3ffa7461d445c8d6c88b765feba0051
Romeosky <skyordive@gmail.com>
2020-04-14 23:12:46 -0600

    fix sqlalchemy

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
 import logging, os, datetime, subprocess
 import urllib.request
-import io, base64, json
+import io, base64, json, sqlalchemy
 from werkzeug.local import LocalProxy
 from collections import Counter
 from joblib import dump, load
